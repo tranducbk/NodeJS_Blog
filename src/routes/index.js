740af26ed4c119e0da514bd61bd29ff59bcedd82
tranducbk <tranducbk04@gmail.com>
2024-10-31 22:57:45 +0700

    Hoan thanh Routes va Controllers

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+const newsRouter = require('./news');
+
+function route(app) {
+    app.use('/news', newsRouter);
+
+    app.get('/', (req, res) => {
+        res.render('home');
+    });
+
+    app.get('/search', (req, res) => {
+        res.render('search');
+    });
+
+    app.post('/search', (req, res) => {
+  
+        console.log(req.body)
+        
+        res.send('')
+      });
+      
+}
+
+module.exports = route;
