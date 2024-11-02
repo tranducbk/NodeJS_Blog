389f61d86a0eb376bddec0601562729cee2e3eb6
Tran Duc <tranducbk04@gmail.com>
2024-11-02 08:55:33 +0700

    Done read DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,21 +1,25 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose.js');
 
 class SiteController {
     // [GET] /
-    async index(req, res) {
+    async index(req, res, next) {
         try {
-          const courses = await Course.find({});
-          res.json(courses);
-        } catch (error) {
-          res.status(400).json({ err: "ERROR!!!" });
+            const courses = await Course.find({});
+
+            // Sử dụng hàm mutipleMongooseToObject để chuyển đổi
+            res.render('home', {
+                courses: mutipleMongooseToObject(courses),
+            });
+        } catch (err) {
+            next(err);
         }
-      }
+    }
 
     // [GET] /search
     search(req, res) {
         res.render('search');
     }
-
 }
 
 module.exports = new SiteController();
